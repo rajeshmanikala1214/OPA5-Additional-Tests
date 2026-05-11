d34b4399af1d470a657ec9ea2877e050740f4bef
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-05-11 19:29:12 +0530

    changes in all test configuration

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -7,7 +7,7 @@ sap.ui.define([
 
 	Opa5.extendConfig({
 		arrangements: new Startup(),
-		viewNamespace: "ns.HTML5Module.view.",
+		viewNamespace: "ns/HTML5Module/view.",
 		autoWait: true
 	});
 });
